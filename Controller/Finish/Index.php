b7efd66ae237ce458bba771e564e05de8c012552
Vladimir Kerkhoff <v.kerkhoff@genmato.com>
2015-12-29 12:33:51 +0100

    Restore cart after canceled payment

--- Controller/Finish/Index.php
+++ Controller/Finish/Index.php
@@ -5,6 +5,9 @@
 
 namespace Paynl\Payment\Controller\Finish;
 
+use Magento\Checkout\Model\Session;
+use Magento\Sales\Model\Order\Email\Sender\OrderSender;
+
 /**
  * Description of Redirect
  *
@@ -24,64 +27,77 @@ class Index extends \Magento\Framework\App\Action\Action
      */
     protected $_orderFactory;
 
+    /**
+     * @var Session
+     */
+    protected $_checkoutSession;
+
+    /**
+     * Index constructor.
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Paynl\Payment\Model\Config $config
+     * @param Session $checkoutSession
+     * @param OrderSender $orderSender
+     * @param \Magento\Sales\Model\OrderFactory $orderFactory
+     */
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Paynl\Payment\Model\Config $config,
+        Session $checkoutSession,
+        OrderSender $orderSender,
         \Magento\Sales\Model\OrderFactory $orderFactory
     )
     {
         $this->_config = $config;
+        $this->_checkoutSession = $checkoutSession;
+        $this->orderSender = $orderSender;
         $this->_orderFactory = $orderFactory;
 
         parent::__construct($context);
     }
 
-    private function _reorder($orderId)
-    {
-        /** @var \Magento\Sales\Model\Order $order */
-        $order = $this->_orderFactory->create()->loadByIncrementId($orderId);;
-        /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
-        $resultRedirect = $this->resultRedirectFactory->create();
-
-        /* @var $cart \Magento\Checkout\Model\Cart */
-        $cart = $this->_objectManager->get('Magento\Checkout\Model\Cart');
-        $cart->truncate();
-
-        $items = $order->getItemsCollection();
-        foreach ($items as $item) {
-            try {
-                $cart->addOrderItem($item);
-            } catch (\Magento\Framework\Exception\LocalizedException $e) {
-                if ($this->_objectManager->get('Magento\Checkout\Model\Session')->getUseNotice(true)) {
-                    $this->messageManager->addNotice($e->getMessage());
-                } else {
-                    $this->messageManager->addError($e->getMessage());
-                }
-                return $resultRedirect->setPath('*/*/history');
-            } catch (\Exception $e) {
-                $this->messageManager->addException($e,
-                    __('We can\'t add this item to your shopping cart right now.'));
-                return $resultRedirect->setPath('checkout/cart');
-            }
-        }
-
-        $cart->save();
-        return $resultRedirect->setPath('checkout/cart');
-    }
 
     public function execute()
     {
         \Paynl\Config::setApiToken($this->_config->getApiToken());
-
         $transaction = \Paynl\Transaction::getForReturn();
 
+        /** @var \Magento\Sales\Model\Order $order */
+        $order = $this->_getCheckoutSession()->getLastRealOrder();
+
+        $resultRedirect = $this->resultRedirectFactory->create();
         if ($transaction->isPaid() || $transaction->isPending()) {
-            $resultRedirect = $this->resultRedirectFactory->create();
-            return $resultRedirect->setPath('checkout/onepage/success');
+            $resultRedirect->setPath('checkout/onepage/success');
         } else {
-            //canceled, reorder
-            $this->messageManager->addNotice(__('Payment canceled'));
-            return $this->_reorder($transaction->getDescription());
+            //canceled, re-activate quote
+            try {
+                // if there is an order - cancel it
+                /** @var \Magento\Sales\Model\Order $order */
+                $order = $this->_getCheckoutSession()->getLastRealOrder();
+                if ($order && $order->getId() && $order->getQuoteId() == $this->_getCheckoutSession()->getQuoteId()) {
+                    $order->cancel()->save();
+                    $this->_getCheckoutSession()->restoreQuote();
+                    $this->messageManager->addNotice(__('Payment canceled'));
+                } else {
+                    $this->messageManager->addNotice(__('Payment canceled, but unable to cancel order'));
+                }
+            } catch (\Magento\Framework\Exception\LocalizedException $e) {
+                $this->messageManager->addExceptionMessage($e, $e->getMessage());
+            } catch (\Exception $e) {
+                $this->messageManager->addExceptionMessage($e, __('Unable to cancel order'));
+            }
+            $resultRedirect->setPath('checkout/cart');
         }
+        return $resultRedirect;
+    }
+
+    /**
+     * Return checkout session object
+     *
+     * @return Session
+     */
+    protected function _getCheckoutSession()
+    {
+        return $this->_checkoutSession;
     }
 }
