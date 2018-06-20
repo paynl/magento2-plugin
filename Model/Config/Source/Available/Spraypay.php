66396b5d91657f0a3fc3dd43ecfd6793d72eadec
Jorn Bakker <jorn@pay.nl>
2018-06-20 11:13:05 +0200

    Added paymentMethods Cashly, SprayPay and Wechat Pay

---
+++ Model/Config/Source/Available/Spraypay.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright © 2015 Pay.nl All rights reserved.
+ */
+
+namespace Paynl\Payment\Model\Config\Source\Available;
+
+
+class Spraypay extends Available
+{
+    protected $_code = 'paynl_payment_spraypay';
+}
