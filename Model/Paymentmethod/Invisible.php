fb6a0ee9ba2e66e27ba0dde49c4fcc65f11db7d8
andypieters <andy@pay.nl>
2018-11-09 13:58:57 +0100

    Created invisible payment method because the general pay.nl settings need to have a model.

---
+++ Model/Paymentmethod/Invisible.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Paynl\Payment\Model\Paymentmethod;
+
+/**
+ * Invisible payment method.
+ * Needs to be set as a model for the pay.nl basic settings, because a model is required
+ * Doesn't do anything
+ */
+class Invisible extends PaymentMethod
+{
+    protected $_canUseCheckout = false;
+    protected $_canUseInternal = false;
+    protected $_code = 'paynl_payment_invisible';
+
+}
