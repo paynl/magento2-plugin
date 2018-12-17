e59be682ad11baed8286e18231ead75e676c0a6b
andypieters <andy@pay.nl>
2018-12-17 21:26:21 +0100

    Added new paymentmethod Alipay

---
+++ Model/Config/Source/Available/Alipay.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright © 2015 Pay.nl All rights reserved.
+ */
+
+namespace Paynl\Payment\Model\Config\Source\Available;
+
+
+class Alipay extends Available
+{
+    protected $_class = \Paynl\Payment\Model\Paymentmethod\Alipay::class;
+}
---
+++ Model/Paymentmethod/Alipay.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Paynl\Payment\Model\Paymentmethod;
+
+
+class Alipay extends PaymentMethod
+{
+    protected $_code = 'paynl_payment_alipay';
+
+    protected function getDefaultPaymentOptionId()
+    {
+        return 2080;
+    }
+}
