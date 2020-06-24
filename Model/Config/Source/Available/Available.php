afa51de3cadde5f4bebd7501069bcc03fa73693e
Kevin Verschoor <k.verschoor@pay.nl>
2020-06-24 15:41:05 +0200

    Update formatting

--- Model/Config/Source/Available/Available.php
+++ Model/Config/Source/Available/Available.php
@@ -6,15 +6,15 @@
 namespace Paynl\Payment\Model\Config\Source\Available;
 
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Config\Storage\WriterInterface;
 use Magento\Framework\App\RequestInterface;
-use \Magento\Framework\Option\ArrayInterface;
 use Magento\Payment\Model\Method\Factory as PaymentMethodFactory;
 use Magento\Store\Model\ScopeInterface;
 use Magento\Store\Model\Store;
-use Magento\Framework\App\Config\Storage\WriterInterface;
-use \Paynl\Payment\Model\Config;
 use Paynl\Payment\Model\Paymentmethod\PaymentMethod;
+use \Magento\Framework\Option\ArrayInterface;
 use \Paynl\Paymentmethods;
+use \Paynl\Payment\Model\Config;
 
 abstract class Available implements ArrayInterface
 {
@@ -42,18 +42,18 @@ abstract class Available implements ArrayInterface
      */
     protected $_config;
 
-    /** 
-     * @var  Store 
+    /**
+     * @var  Store
      * */
-    private $store;   
+    private $store;
 
-    /** 
-     * @var  ConfigWriter 
+    /**
+     * @var  ConfigWriter
      * */
     private $configWriter;
-    
-    /** 
-     * @var  CacheTypeList 
+
+    /**
+     * @var  CacheTypeList
      * */
     private $cacheTypeList;
 
@@ -62,11 +62,10 @@ abstract class Available implements ArrayInterface
         RequestInterface $request,
         ScopeConfigInterface $scopeConfig,
         PaymentMethodFactory $paymentMethodFactory,
-        Store $store,  
+        Store $store,
         WriterInterface $configWriter,
         \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList
-    )
-    {
+    ) {
         $this->_config = $config;
         $this->_request = $request;
         $this->_scopeConfig = $scopeConfig;
@@ -75,7 +74,6 @@ abstract class Available implements ArrayInterface
         $this->configWriter = $configWriter;
         $this->cacheTypeList = $cacheTypeList;
     }
-
 
     /**
      * Options getter
@@ -122,7 +120,7 @@ abstract class Available implements ArrayInterface
         $serviceId = trim($this->getConfigValue('payment/paynl/serviceid'));
         $tokencode = trim($this->getConfigValue('payment/paynl/tokencode'));
 
-        if(! empty($tokencode)) {
+        if (!empty($tokencode)) {
             \Paynl\Config::setTokenCode($tokencode);
         }
 
@@ -139,12 +137,11 @@ abstract class Available implements ArrayInterface
     protected function getPaymentOptionId()
     {
         $method = $this->_paymentmethodFactory->create($this->_class);
-        if($method instanceof PaymentMethod){
+        if ($method instanceof PaymentMethod) {
             return $method->getPaymentOptionId();
         }
         return null;
     }
-
 
     protected function getConfigValue($path)
     {
@@ -160,7 +157,6 @@ abstract class Available implements ArrayInterface
             $scopeValue = $website;
             $scopeType = ScopeInterface::SCOPE_WEBSITE;
         }
-
 
         return $this->_scopeConfig->getValue($path, $scopeType, $scopeValue);
     }
@@ -182,88 +178,89 @@ abstract class Available implements ArrayInterface
         return false;
     }
 
-    public function setDefaults($Paymentmethods){        
-        
+    public function setDefaults($Paymentmethods)
+    {
+
         $Magento_paymentmethod_codes = array(
-            "0"=>"paylink",
-            "739"=>"afterpay",
-            "2080"=>"alipay",
-            "1705"=>"amex",
-            "2277"=>"applepay",
-            "1672"=>"billink",
-            "1744"=>"capayable",
-            "1813"=>"capayable_gespreid",
-            "1945"=>"cartasi",
-            "710"=>"cartebleue",
-            "1981"=>"cashly",
-            "139"=>"clickandbuy",
-            "2107"=>"creditclick",
-            "1939"=>"dankort",
-            "2062"=>"eps",
-            "815"=>"fashioncheque",
-            "1699"=>"fashiongiftcard",
-            "1702"=>"focum",
-            "812"=>"gezondheidsbon",
-            "694"=>"giropay",
-            "1657"=>"givacard",
-            "2283"=>"huisentuincadeau",
-            "10"=>"ideal",
-            "1729"=>"instore",
-            "1717"=>"klarna",
-            "2265"=>"klarnakp",
-            "712"=>"maestro",
-            "436"=>"mistercash",
-            "2271"=>"multibanco",
-            "1588"=>"mybank",
-            "136"=>"overboeking",
-            "2379"=>"payconiq",
-            "138"=>"paypal",
-            "553"=>"paysafecard",
-            "816"=>"podiumcadeaukaart",
-            "707"=>"postepay",
-            "2151"=>"przelewy24",
-            "559"=>"sofortbanking",
-            "1987"=>"spraypay",
-            "1600"=>"telefonischbetalen",
-            "2104"=>"tikkie",
-            "706"=>"visamastercard",
-            "1714"=>"vvvgiftcard",
-            "811"=>"webshopgiftcard",
-            "1978"=>"wechatpay",
-            "1666"=>"wijncadeau",
-            "1877"=>"yehhpay",
-            "1645"=>"yourgift"
+            "0"    => "paylink",
+            "739"  => "afterpay",
+            "2080" => "alipay",
+            "1705" => "amex",
+            "2277" => "applepay",
+            "1672" => "billink",
+            "1744" => "capayable",
+            "1813" => "capayable_gespreid",
+            "1945" => "cartasi",
+            "710"  => "cartebleue",
+            "1981" => "cashly",
+            "139"  => "clickandbuy",
+            "2107" => "creditclick",
+            "1939" => "dankort",
+            "2062" => "eps",
+            "815"  => "fashioncheque",
+            "1699" => "fashiongiftcard",
+            "1702" => "focum",
+            "812"  => "gezondheidsbon",
+            "694"  => "giropay",
+            "1657" => "givacard",
+            "2283" => "huisentuincadeau",
+            "10"   => "ideal",
+            "1729" => "instore",
+            "1717" => "klarna",
+            "2265" => "klarnakp",
+            "712"  => "maestro",
+            "436"  => "mistercash",
+            "2271" => "multibanco",
+            "1588" => "mybank",
+            "136"  => "overboeking",
+            "2379" => "payconiq",
+            "138"  => "paypal",
+            "553"  => "paysafecard",
+            "816"  => "podiumcadeaukaart",
+            "707"  => "postepay",
+            "2151" => "przelewy24",
+            "559"  => "sofortbanking",
+            "1987" => "spraypay",
+            "1600" => "telefonischbetalen",
+            "2104" => "tikkie",
+            "706"  => "visamastercard",
+            "1714" => "vvvgiftcard",
+            "811"  => "webshopgiftcard",
+            "1978" => "wechatpay",
+            "1666" => "wijncadeau",
+            "1877" => "yehhpay",
+            "1645" => "yourgift",
         );
 
         $changed = false;
 
         foreach ($Paymentmethods as $key => $method) {
-            
-            if(isset($Magento_paymentmethod_codes[$method['id']])){
+
+            if (isset($Magento_paymentmethod_codes[$method['id']])) {
                 $name = $Magento_paymentmethod_codes[$method['id']];
 
-                if(strlen($this->store->getConfig('payment/paynl_payment_'.$name.'/min_order_total')) == 0 && isset($method['min_amount'])){
-                    $this->configWriter->save('payment/paynl_payment_'.$name.'/min_order_total', $method['min_amount']);
-                    if(strlen($method['min_amount']) > 0){$changed = true;}
+                if (strlen($this->store->getConfig('payment/paynl_payment_' . $name . '/min_order_total')) == 0 && isset($method['min_amount'])) {
+                    $this->configWriter->save('payment/paynl_payment_' . $name . '/min_order_total', $method['min_amount']);
+                    if (strlen($method['min_amount']) > 0) {$changed = true;}
                 }
 
-                if(strlen($this->store->getConfig('payment/paynl_payment_'.$name.'/max_order_total')) == 0 && isset($method['max_amount'])){
-                    $this->configWriter->save('payment/paynl_payment_'.$name.'/max_order_total', $method['max_amount']);
-                    if(strlen($method['max_amount']) > 0){$changed = true;}
+                if (strlen($this->store->getConfig('payment/paynl_payment_' . $name . '/max_order_total')) == 0 && isset($method['max_amount'])) {
+                    $this->configWriter->save('payment/paynl_payment_' . $name . '/max_order_total', $method['max_amount']);
+                    if (strlen($method['max_amount']) > 0) {$changed = true;}
                 }
 
-                if(strlen($this->store->getConfig('payment/paynl_payment_'.$name.'/instructions')) == 0 && isset($method['brand']['public_description'])){
-                    $this->configWriter->save('payment/paynl_payment_'.$name.'/instructions', $method['brand']['public_description']);
-                    if(strlen($method['brand']['public_description']) > 0){$changed = true;}
+                if (strlen($this->store->getConfig('payment/paynl_payment_' . $name . '/instructions')) == 0 && isset($method['brand']['public_description'])) {
+                    $this->configWriter->save('payment/paynl_payment_' . $name . '/instructions', $method['brand']['public_description']);
+                    if (strlen($method['brand']['public_description']) > 0) {$changed = true;}
                 }
-            }            
+            }
         }
-        
+
         //Clean the cache or esle it won't show the changes
         $this->cacheTypeList->cleanType(\Magento\Framework\App\Cache\Type\Config::TYPE_IDENTIFIER);
 
         //Refresh the page to apply the defaults after opening Payment methodes
-        if($changed){
+        if ($changed) {
             header("Refresh:0");
         }
 
