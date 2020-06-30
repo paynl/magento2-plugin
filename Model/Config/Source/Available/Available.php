7a649da675b64a205997e7950c0b5c126a9b4346
Kevin Verschoor <k.verschoor@pay.nl>
2020-06-30 08:47:50 +0200

    fixes

--- Model/Config/Source/Available/Available.php
+++ Model/Config/Source/Available/Available.php
@@ -57,6 +57,11 @@ abstract class Available implements ArrayInterface
      * */
     private $cacheTypeList;
 
+    /**
+     * @var  Changed
+     * */
+    private $changed;
+
     public function __construct(
         Config $config,
         RequestInterface $request,
@@ -65,7 +70,8 @@ abstract class Available implements ArrayInterface
         Store $store,
         WriterInterface $configWriter,
         \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList
-    ) {
+    )
+    {
         $this->_config = $config;
         $this->_request = $request;
         $this->_scopeConfig = $scopeConfig;
@@ -82,6 +88,7 @@ abstract class Available implements ArrayInterface
      */
     public function toOptionArray()
     {
+        $this->setDefaults($list);
         $arrOptions = $this->toArray();
 
         $arrResult = [];
@@ -168,7 +175,6 @@ abstract class Available implements ArrayInterface
             $paymentOptionId = $this->getPaymentOptionId();
 
             $list = Paymentmethods::getList();
-            $this->setDefaults($list);
 
             if (isset($list[$paymentOptionId])) {
                 return true;
@@ -178,91 +184,99 @@ abstract class Available implements ArrayInterface
         return false;
     }
 
-    public function setDefaults($Paymentmethods)
+    public function setDefaults()
     {
+        $configured = $this->configureSDK();
+        if ($configured) {
+            $Paymentmethods = Paymentmethods::getList();
+            $Magento_paymentmethod_codes = array(
+                "0" => "paylink",
+                "739" => "afterpay",
+                "2080" => "alipay",
+                "1705" => "amex",
+                "2277" => "applepay",
+                "1672" => "billink",
+                "1744" => "capayable",
+                "1813" => "capayable_gespreid",
+                "1945" => "cartasi",
+                "710" => "cartebleue",
+                "1981" => "cashly",
+                "139" => "clickandbuy",
+                "2107" => "creditclick",
+                "1939" => "dankort",
+                "2062" => "eps",
+                "815" => "fashioncheque",
+                "1699" => "fashiongiftcard",
+                "1702" => "focum",
+                "812" => "gezondheidsbon",
+                "694" => "giropay",
+                "1657" => "givacard",
+                "2283" => "huisentuincadeau",
+                "10" => "ideal",
+                "1729" => "instore",
+                "1717" => "klarna",
+                "2265" => "klarnakp",
+                "712" => "maestro",
+                "436" => "mistercash",
+                "2271" => "multibanco",
+                "1588" => "mybank",
+                "136" => "overboeking",
+                "2379" => "payconiq",
+                "138" => "paypal",
+                "553" => "paysafecard",
+                "816" => "podiumcadeaukaart",
+                "707" => "postepay",
+                "2151" => "przelewy24",
+                "559" => "sofortbanking",
+                "1987" => "spraypay",
+                "1600" => "telefonischbetalen",
+                "2104" => "tikkie",
+                "706" => "visamastercard",
+                "1714" => "vvvgiftcard",
+                "811" => "webshopgiftcard",
+                "1978" => "wechatpay",
+                "1666" => "wijncadeau",
+                "1877" => "yehhpay",
+                "1645" => "yourgift",
+            );
 
-        $Magento_paymentmethod_codes = array(
-            "0"    => "paylink",
-            "739"  => "afterpay",
-            "2080" => "alipay",
-            "1705" => "amex",
-            "2277" => "applepay",
-            "1672" => "billink",
-            "1744" => "capayable",
-            "1813" => "capayable_gespreid",
-            "1945" => "cartasi",
-            "710"  => "cartebleue",
-            "1981" => "cashly",
-            "139"  => "clickandbuy",
-            "2107" => "creditclick",
-            "1939" => "dankort",
-            "2062" => "eps",
-            "815"  => "fashioncheque",
-            "1699" => "fashiongiftcard",
-            "1702" => "focum",
-            "812"  => "gezondheidsbon",
-            "694"  => "giropay",
-            "1657" => "givacard",
-            "2283" => "huisentuincadeau",
-            "10"   => "ideal",
-            "1729" => "instore",
-            "1717" => "klarna",
-            "2265" => "klarnakp",
-            "712"  => "maestro",
-            "436"  => "mistercash",
-            "2271" => "multibanco",
-            "1588" => "mybank",
-            "136"  => "overboeking",
-            "2379" => "payconiq",
-            "138"  => "paypal",
-            "553"  => "paysafecard",
-            "816"  => "podiumcadeaukaart",
-            "707"  => "postepay",
-            "2151" => "przelewy24",
-            "559"  => "sofortbanking",
-            "1987" => "spraypay",
-            "1600" => "telefonischbetalen",
-            "2104" => "tikkie",
-            "706"  => "visamastercard",
-            "1714" => "vvvgiftcard",
-            "811"  => "webshopgiftcard",
-            "1978" => "wechatpay",
-            "1666" => "wijncadeau",
-            "1877" => "yehhpay",
-            "1645" => "yourgift",
-        );
+            $this->changed = false;
 
-        $changed = false;
+            foreach ($Paymentmethods as $key => $method) {
 
-        foreach ($Paymentmethods as $key => $method) {
+                if (isset($Magento_paymentmethod_codes[$method['id']])) {
+                    $name = $Magento_paymentmethod_codes[$method['id']];
 
-            if (isset($Magento_paymentmethod_codes[$method['id']])) {
-                $name = $Magento_paymentmethod_codes[$method['id']];
-
-                if (strlen($this->store->getConfig('payment/paynl_payment_' . $name . '/min_order_total')) == 0 && isset($method['min_amount'])) {
-                    $this->configWriter->save('payment/paynl_payment_' . $name . '/min_order_total', $method['min_amount']);
-                    if (strlen($method['min_amount']) > 0) {$changed = true;}
+                    if (isset($method['min_amount'])) {
+                        $this->setDefaultvalue('payment/paynl_payment_' . $name . '/min_order_total', $method['min_amount']);
+                    }
+                    if (isset($method['max_amount'])) {
+                        $this->setDefaultvalue('payment/paynl_payment_' . $name . '/max_order_total', $method['max_amount']);
+                    }
+                    if (isset($method['brand']['public_description'])) {
+                        $this->setDefaultvalue('payment/paynl_payment_' . $name . '/instructions', $method['brand']['public_description']);
+                    }
                 }
+            }
 
-                if (strlen($this->store->getConfig('payment/paynl_payment_' . $name . '/max_order_total')) == 0 && isset($method['max_amount'])) {
-                    $this->configWriter->save('payment/paynl_payment_' . $name . '/max_order_total', $method['max_amount']);
-                    if (strlen($method['max_amount']) > 0) {$changed = true;}
-                }
+            //Clean the cache or esle it won't show the changes
+            $this->cacheTypeList->cleanType(\Magento\Framework\App\Cache\Type\Config::TYPE_IDENTIFIER);
 
-                if (strlen($this->store->getConfig('payment/paynl_payment_' . $name . '/instructions')) == 0 && isset($method['brand']['public_description'])) {
-                    $this->configWriter->save('payment/paynl_payment_' . $name . '/instructions', $method['brand']['public_description']);
-                    if (strlen($method['brand']['public_description']) > 0) {$changed = true;}
-                }
+            //Refresh the page to apply the defaults after opening Payment methodes
+            if ($this->changed) {
+                header("Refresh:0");
             }
         }
 
-        //Clean the cache or esle it won't show the changes
-        $this->cacheTypeList->cleanType(\Magento\Framework\App\Cache\Type\Config::TYPE_IDENTIFIER);
+    }
 
-        //Refresh the page to apply the defaults after opening Payment methodes
-        if ($changed) {
-            header("Refresh:0");
+    private function setDefaultvalue($configName, $value)
+    {
+        if (strlen($this->store->getConfig($configName)) == 0) {
+            $this->configWriter->save($configName, $value);
+            if (strlen($value) > 0) {
+                $this->changed = true;
+            }
         }
-
     }
 }
