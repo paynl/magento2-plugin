985c7cdeca86b8f24d4e2372147a1a8d3cf70d12
Vasya Danylyuk <vvvasya@gmail.com>
2020-07-23 10:19:42 +0300

    Small fix for dob and gender fields if they are using at checkout

---
+++ Plugin/OrderPaymentAdditionalInformation.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Paynl\Payment\Plugin;
+
+use Magento\Sales\Model\Order\Payment;
+
+/**
+ * Class OrderPaymentAdditionalInformation
+ * @package Paynl\Payment\Plugin
+ */
+class OrderPaymentAdditionalInformation
+{
+    /**
+     * @param Payment $subject
+     * @param $result
+     * @return array
+     * @throws \Exception
+     */
+    public function afterGetAdditionalInformation(Payment $subject, $result)
+    {
+        /** @var Payment $subject */
+        if (is_array($result)) {
+            $order = $subject->getOrder();
+            if (empty($result['dob']) && $order->getCustomerDob()) {
+                $result['dob'] = (new \DateTime($order->getCustomerDob()))->format('Y-m-d');
+            }
+            if (empty($result['gender']) && $order->getCustomerGender()) {
+                $gender = $order->getCustomerGender();
+                switch ($gender) {
+                    case '1':
+                        $gender = 'M';
+                        break;
+                    case '2':
+                        $gender = 'F';
+                        break;
+                    default:
+                        $gender = null;
+                        break;
+                }
+                if (!empty($gender)) {
+                    $result['gender'] = $gender;
+                }
+            }
+        }
+        return $result;
+    }
+}
