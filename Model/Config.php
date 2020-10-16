edfa9d644ea9e97c8292f3878a98cc81e7fff0a9
Anne <anne@pay.nl>
2020-10-16 15:19:42 +0200

    Added docblock and autoformatting

--- Model/Config.php
+++ Model/Config.php
@@ -20,10 +20,15 @@ class Config
 
     public function __construct(
         Store $store
-    ) {
+    )
+    {
         $this->store = $store;
     }
 
+    /**
+     * @param string $defaultValue
+     * @return mixed|string
+     */
     public function getVersion($defaultValue = '')
     {
         $composerFilePath = sprintf('%s/%s', rtrim(__DIR__, '/'), '../composer.json');
@@ -47,7 +52,8 @@ class Config
         return $version;
     }
 
-    public function getPHPVersion(){
+    public function getPHPVersion()
+    {
         return substr(phpversion(), 0, 3);
     }
 
@@ -66,22 +72,22 @@ class Config
 
     public function isTestMode()
     {
-        $remoteIP =  isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
+        $remoteIP = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
         $ip = isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : $remoteIP;
-        
+
         $ipconfig = $this->store->getConfig('payment/paynl/testipaddress');
-        $allowed_ips = explode(',', $ipconfig);       
-        if(in_array($ip, $allowed_ips) && filter_var($ip, FILTER_VALIDATE_IP) && strlen($ip) > 0 && count($allowed_ips) > 0){
+        $allowed_ips = explode(',', $ipconfig);
+        if (in_array($ip, $allowed_ips) && filter_var($ip, FILTER_VALIDATE_IP) && strlen($ip) > 0 && count($allowed_ips) > 0) {
             return true;
-        }        
+        }
         return $this->store->getConfig('payment/paynl/testmode') == 1;
     }
-    
+
     public function isSendDiscountTax()
     {
         return $this->store->getConfig('payment/paynl/discount_tax') == 1;
     }
-    
+
     public function isNeverCancel()
     {
         return $this->store->getConfig('payment/paynl/never_cancel') == 1;
@@ -108,13 +114,14 @@ class Config
     {
         return $this->store->getConfig('payment/' . $methodCode . '/order_status');
     }
-    
+
     public function getAuthorizedStatus($methodCode)
     {
         return $this->store->getConfig('payment/' . $methodCode . '/order_status_authorized');
     }
 
-    public function getPaidStatus($methodCode){
+    public function getPaidStatus($methodCode)
+    {
         return $this->store->getConfig('payment/' . $methodCode . '/order_status_processing');
     }
 
@@ -122,9 +129,10 @@ class Config
      * @param $methodCode string
      * @return string
      */
-    public function getSuccessPage($methodCode){
+    public function getSuccessPage($methodCode)
+    {
         $success_page = $this->store->getConfig('payment/' . $methodCode . '/custom_success_page');
-        if(empty($success_page)) $success_page = 'checkout/onepage/success';
+        if (empty($success_page)) $success_page = 'checkout/onepage/success';
 
         return $success_page;
     }
@@ -136,15 +144,15 @@ class Config
      */
     public function configureSDK()
     {
-        $apiToken  = $this->getApiToken();
+        $apiToken = $this->getApiToken();
         $serviceId = $this->getServiceId();
         $tokencode = $this->getTokencode();
 
-        if(! empty($tokencode)) {
+        if (!empty($tokencode)) {
             \Paynl\Config::setTokenCode($tokencode);
         }
 
-        if ( ! empty($apiToken) && ! empty($serviceId)) {
+        if (!empty($apiToken) && !empty($serviceId)) {
             \Paynl\Config::setApiToken($apiToken);
             \Paynl\Config::setServiceId($serviceId);
 
@@ -169,15 +177,15 @@ class Config
         return trim($this->store->getConfig('payment/paynl/serviceid'));
     }
 
-    public function getIconUrl() 
+    public function getIconUrl()
     {
         $url = 'https://static.pay.nl/payment_profiles/50x32/#paymentOptionId#.png';
         $iconUrl = trim($this->store->getConfig('payment/paynl/iconurl'));
 
-        return empty($iconUrl)?$url:$iconUrl;
+        return empty($iconUrl) ? $url : $iconUrl;
     }
-    
-    public function getCancelURL() 
+
+    public function getCancelURL()
     {
         return $this->store->getConfig('payment/paynl/cancelurl');
     }
