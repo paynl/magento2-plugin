585feb9cca50344e70325c24f2890b437cb7396c
woutse <wouter@pay.nl>
2021-01-28 17:08:26 +0100

    Added logging and some updates

--- Observer/OrderSaveCommitAfter.php
+++ Observer/OrderSaveCommitAfter.php
@@ -7,6 +7,7 @@ use Magento\Store\Model\Store;
 use Psr\Log\LoggerInterface;
 use Paynl\Result\Transaction\Transaction;
 use Paynl\Payment\Model\Config;
+use Magento\Sales\Model\Order;
 
 class OrderSaveCommitAfter implements ObserverInterface
 {
@@ -42,29 +43,32 @@ class OrderSaveCommitAfter implements ObserverInterface
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         $order = $observer->getEvent()->getOrder();
+        $bAutoCapture = $this->store->getConfig('payment/paynl/auto_capture') == 1;
 
-        $auto_capture = $this->store->getConfig('payment/paynl/auto_capture');
-
-        if (!$order->hasInvoices() && $order->hasShipments() && $order->getState() == 'processing' && $auto_capture == 1) {
+        if ($bAutoCapture && $order->getState() == Order::STATE_PROCESSING && !$order->hasInvoices() && $order->hasShipments()) {
             $payment = $order->getPayment();
             $data = $payment->getData();
+
             if (isset($data['last_trans_id'])) {
-                $transactionId = $data['last_trans_id'];
-                $amountAuthorized = isset($data['base_amount_authorized']) ? (float)$data['base_amount_authorized'] : null;
+                $bHasAmountAuthorized = !empty($data['base_amount_authorized']);
                 $amountPaid = isset($data['amount_paid']) ? $data['amount_paid'] : null;
                 $amountRefunded = isset($data['amount_refunded']) ? $data['amount_refunded'] : null;
-                if ($amountAuthorized > 0 && !is_null($amountAuthorized) && is_null($amountPaid) && is_null($amountRefunded) && !empty($transactionId) && !is_null($transactionId)) {
-                    $order->addStatusHistoryComment(__('PAY. -Order has been shipped, but no invoice was found. Capturing the payment automatically.'), false);
+
+                if ($bHasAmountAuthorized && is_null($amountPaid) && is_null($amountRefunded)) {
+                    $this->logger->debug('PAY.: CAPTURING ' . $data['last_trans_id']);
                     try {
                         \Paynl\Config::setApiToken($this->config->getApiToken());
-                        $result = \Paynl\Transaction::capture($transactionId);
-                    } catch (\Paynl\Error\Error $e) {
-                        $this->logger->notice('Order PAY error: ' . $e->getMessage() . ' EntityId: ' . $order->getEntityId());
+                        $result = \Paynl\Transaction::capture($data['last_trans_id']);
+                        $strResult = 'Success';
+                    } catch (\Exception $e) {
+                        $this->logger->debug('Order PAY error: ' . $e->getMessage() . ' EntityId: ' . $order->getEntityId());
+                        $strResult = 'Failed. Errorcode: PAY-MAGENTO2-003. See docs.pay.nl for more information';
                     }
+
+                    $order->addStatusHistoryComment(__('PAY. - Performed auto-capture. Result: ' . $strResult), false)->save();
                 }
             }
         }
-
-        $this->logger->notice('Order PAY Status:' . $order->getStatus() . ' Order PAY State:' . $order->getState() . ' EntityId: ' . $order->getEntityId());
     }
+
 }
