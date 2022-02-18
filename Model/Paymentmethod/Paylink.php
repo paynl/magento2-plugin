2a38b09732cb8b12ffd6c8973fb5d61b80fe7e94
k.verschoor@pay.nl <k.verschoor@pay.nl>
2022-02-18 10:53:49 +0100

    PLUG-833 Paylink fixes & Add setting to disable Paylink emails

--- Model/Paymentmethod/Paylink.php
+++ Model/Paymentmethod/Paylink.php
@@ -49,90 +49,92 @@ class Paylink extends PaymentMethod
                 $url = substr_replace($url, strtoupper($lang), $pos, strlen('NL'));
             }
 
-            $paylinktext = __('A PAY. Paylink has been send to');
-
-            $order->addStatusHistoryComment($paylinktext . ' ' . $order->getCustomerEmail() . '.', $status);
-
             $store = $order->getStore();
             $storeId = $order->getStoreId();
+            $send_paylink_email = $this->_scopeConfig->getValue('payment/paynl_payment_paylink/send_paylink_email', 'store', $storeId);
 
-            $supportEmail = $this->_scopeConfig->getValue('trans_email/ident_support/email', 'store', $storeId);
-            $senderName = $this->_scopeConfig->getValue('trans_email/ident_sales/name', 'store', $storeId);
-            $senderEmail = $this->_scopeConfig->getValue('trans_email/ident_sales/email', 'store', $storeId);
-
-            $sender = [
-                'name' => $senderName,
-                'email' => $senderEmail,
-            ];
-
-            $customerEmail = [$order->getCustomerEmail()];
-
-            $paymentHelper = $objectManager->create(\Magento\Payment\Helper\Data::class);
-
-            $orderHTML = $paymentHelper->getInfoBlockHtml(
-                $order->getPayment(),
-                $storeId
-            );
-
-            $addressRenderer = $objectManager->create(\Magento\Sales\Model\Order\Address\Renderer::class);
-
-            $show_order_in_mail = $this->_scopeConfig->getValue('payment/paynl_payment_paylink/show_order_in_mail', 'store', $storeId);
-            if ($show_order_in_mail) {
-                $show_order_in_mail = 1;
+            if ($send_paylink_email == 0) {
+                $paylinktext = __('A PAY. Paylink has been created: ');
+                $order->addStatusHistoryComment($paylinktext . $url, $status);
             } else {
-                $show_order_in_mail = 0;
-            }
+                $paylinktext = __('A PAY. Paylink has been send to');
+                $order->addStatusHistoryComment($paylinktext . ' ' . $order->getCustomerEmail() . '.', $status);
 
-            $subject = $this->_scopeConfig->getValue('payment/paynl_payment_paylink/paylink_subject', 'store', $storeId);
-            $subject = str_replace('((paylink))', '<a href="'.$url.'">'.__('PAY. paylink').'</a>', $subject);
-            $subject = str_replace('((customer_name))', $order->getCustomerName(), $subject);
-            $subject = str_replace('((store_name))', $order->getStore()->getName(), $subject);
-            $subject = str_replace('((support_email))', '<a href="mailto:'.$supportEmail.'">'.$supportEmail.'</a>', $subject);
-            $subject = str_replace('((order_id))', $order->getIncrementId(), $subject);
+                $supportEmail = $this->_scopeConfig->getValue('trans_email/ident_support/email', 'store', $storeId);
+                $senderName = $this->_scopeConfig->getValue('trans_email/ident_sales/name', 'store', $storeId);
+                $senderEmail = $this->_scopeConfig->getValue('trans_email/ident_sales/email', 'store', $storeId);
 
-            $body = $this->_scopeConfig->getValue('payment/paynl_payment_paylink/paylink_body', 'store', $storeId);
-            $body = nl2br($body);
-            $body = str_replace('((paylink))', '<a href="'.$url.'">'.__('PAY. paylink').'</a>', $body);
-            $body = str_replace('((customer_name))', $order->getCustomerName(), $body);
-            $body = str_replace('((store_name))', $order->getStore()->getName(), $body);
-            $body = str_replace('((support_email))', '<a href="mailto:'.$supportEmail.'">'.$supportEmail.'</a>', $body);
-            $body = str_replace('((order_id))', $order->getIncrementId(), $body);
+                $sender = [
+                    'name' => $senderName,
+                    'email' => $senderEmail,
+                ];
 
-            $templateVars = [
-                'subject' => $subject,
-                'body' => $body,
-                'order' => $order,
-                'store' => $store,
-                'customer_name' =>  $order->getCustomerName(),
-                'paylink' => $url,
-                'support_email' => $supportEmail,
-                'current_language' => $lang,
-                'order_id' =>  $order->getIncrementId(),
-                'billing' => $order->getBillingAddress(),
-                'payment_html' => $orderHTML,
-                'formattedShippingAddress' => $order->getIsVirtual() ? null : $addressRenderer->format($order->getShippingAddress(), 'html'),
-                'formattedBillingAddress' => $addressRenderer->format($order->getBillingAddress(), 'html'),
-                'created_at_formatted' => $order->getCreatedAtFormatted(1),
-                'order_data' => [
+                $customerEmail = [$order->getCustomerEmail()];
+
+                $paymentHelper = $objectManager->create(\Magento\Payment\Helper\Data::class);
+
+                $orderHTML = $paymentHelper->getInfoBlockHtml(
+                    $order->getPayment(),
+                    $storeId
+                );
+
+                $addressRenderer = $objectManager->create(\Magento\Sales\Model\Order\Address\Renderer::class);
+
+                $show_order_in_mail = $this->_scopeConfig->getValue('payment/paynl_payment_paylink/show_order_in_mail', 'store', $storeId);
+                if ($show_order_in_mail) {
+                    $show_order_in_mail = 1;
+                } else {
+                    $show_order_in_mail = 0;
+                }
+
+                $subject = $this->_scopeConfig->getValue('payment/paynl_payment_paylink/paylink_subject', 'store', $storeId);
+                $subject = str_replace('((paylink))', '<a href="' . $url . '">' . __('PAY. paylink') . '</a>', $subject);
+                $subject = str_replace('((customer_name))', $order->getCustomerName(), $subject);
+                $subject = str_replace('((store_name))', $order->getStore()->getName(), $subject);
+                $subject = str_replace('((support_email))', '<a href="mailto:' . $supportEmail . '">' . $supportEmail . '</a>', $subject);
+                $subject = str_replace('((order_id))', $order->getIncrementId(), $subject);
+
+                $body = $this->_scopeConfig->getValue('payment/paynl_payment_paylink/paylink_body', 'store', $storeId);
+                $body = nl2br($body);
+                $body = str_replace('((paylink))', '<a href="' . $url . '">' . __('PAY. paylink') . '</a>', $body);
+                $body = str_replace('((customer_name))', $order->getCustomerName(), $body);
+                $body = str_replace('((store_name))', $order->getStore()->getName(), $body);
+                $body = str_replace('((support_email))', '<a href="mailto:' . $supportEmail . '">' . $supportEmail . '</a>', $body);
+                $body = str_replace('((order_id))', $order->getIncrementId(), $body);
+
+                $templateVars = [
+                    'subject' => $subject,
+                    'body' => $body,
+                    'order' => $order,
+                    'store' => $store,
+                    'customer_name' =>  $order->getCustomerName(),
+                    'paylink' => $url,
+                    'support_email' => $supportEmail,
+                    'current_language' => $lang,
+                    'order_id' =>  $order->getIncrementId(),
+                    'billing' => $order->getBillingAddress(),
+                    'payment_html' => $orderHTML,
+                    'formattedShippingAddress' => $order->getIsVirtual() ? null : $addressRenderer->format($order->getShippingAddress(), 'html'),
+                    'formattedBillingAddress' => $addressRenderer->format($order->getBillingAddress(), 'html'),
+                    'created_at_formatted' => $order->getCreatedAtFormatted(1),
                     'customer_name' => $order->getCustomerName(),
                     'is_not_virtual' => $order->getIsNotVirtual(),
                     'email_customer_note' => $order->getEmailCustomerNote(),
                     'frontend_status_label' => $order->getFrontendStatusLabel(),
-                    'show_order_in_mail' => $show_order_in_mail
-                ],
+                    'show_order_in_mail' => $show_order_in_mail,
+                ];
 
-            ];
+                $transportBuilder = $objectManager->create(\Magento\Framework\Mail\Template\TransportBuilder::class);
 
-            $transportBuilder = $objectManager->create(\Magento\Framework\Mail\Template\TransportBuilder::class);
-
-            $transport = $transportBuilder->setTemplateIdentifier('paylink_email_template')
-            ->setTemplateOptions(['area' => \Magento\Framework\App\Area::AREA_FRONTEND, 'store' => $storeId])
-            ->setTemplateVars($templateVars)
-            ->setFrom($sender)
-            ->addTo($customerEmail)
-            ->setReplyTo($supportEmail)
-            ->getTransport();
-            $transport->sendMessage();
+                $transport = $transportBuilder->setTemplateIdentifier('paylink_email_template')
+                    ->setTemplateOptions(['area' => \Magento\Framework\App\Area::AREA_FRONTEND, 'store' => $storeId])
+                    ->setTemplateVars($templateVars)
+                    ->setFrom($sender)
+                    ->addTo($customerEmail)
+                    ->setReplyTo($supportEmail)
+                    ->getTransport();
+                $transport->sendMessage();
+            }
 
             parent::initialize($paymentAction, $stateObject);
         }
