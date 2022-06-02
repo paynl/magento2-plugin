6cd44bcde388c7b0cc9ddbcdb2c89fcc230f2893
k.verschoor@pay.nl <k.verschoor@pay.nl>
2022-06-02 13:13:42 +0200

    Change dropdown

---
+++ Model/Config/Source/GoogleAnalytics.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Paynl\Payment\Model\Config\Source;
+
+use \Magento\Framework\Option\ArrayInterface;
+
+class GoogleAnalytics implements ArrayInterface
+{
+
+  /**
+   * Options getter
+   *
+   * @return array
+   */
+    public function toOptionArray()
+    {
+        $arrOptions = $this->toArray();
+
+        $arrResult = [];
+        foreach ($arrOptions as $value => $label) {
+            $arrResult[] = ['value' => $value, 'label' => $label];
+        }
+        return $arrResult;
+    }
+
+  /**
+   * Get options in "key-value" format
+   *
+   * @return array
+   */
+    public function toArray()
+    {
+        return [        
+        '1' => __('Transfer analytics to PAY.'),
+        '0' => __('Disable'),
+        ];
+    }
+}
