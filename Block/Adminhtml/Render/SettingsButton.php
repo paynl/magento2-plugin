ba46c0fdf9b032f1840ec47bc91481fd06ad8b4a
k.verschoor@pay.nl <k.verschoor@pay.nl>
2022-08-02 13:59:19 +0200

    Alphabetized Payment methods

--- Block/Adminhtml/Render/SettingsButton.php
+++ Block/Adminhtml/Render/SettingsButton.php
@@ -27,14 +27,10 @@ class SettingsButton extends Field
         $currentUrl = $urlInterface->getCurrentUrl();
         $payUrl = str_replace("payment", "paynl_general", $currentUrl);
 
-        $textTranslated = __('PAY. Settings have been moved to their own tab, click [[here]] to go to the new settings page.');
-        $text = str_replace('[[here]]', '<a href="' . $payUrl . '">here</a>', $textTranslated);
-        $text = str_replace('[[hier]]', '<a href="' . $payUrl . '">hier</a>', $textTranslated);
+        $text = __('PAY. Settings have been moved to their own tab, click') . ' <a href="' . $payUrl . '">' . __('here') . '</a> ' .  __('to go to the new settings page.');
 
         $html = '<tr id="row_' . $element->getHtmlId() . '">';
         $html .= '  <td class="value" style="width:100%;">' . $text . '</td>';
         $html .= '</tr>';
-
-        return $html;
     }
 }
