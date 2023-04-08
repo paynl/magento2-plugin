f1345c70655534941bcb8d7a9fd5a3a6fb9e8bd3
woutse <wouter@pay.nl>
2023-04-08 21:48:50 +0200

    Code polish

--- Helper/PayHelper.php
+++ Helper/PayHelper.php
@@ -213,6 +213,7 @@ class PayHelper extends \Magento\Framework\App\Helper\AbstractHelper
      * @param string $cookieName
      * @phpcs:disable PSR12.Functions.ReturnTypeDeclaration
      * @phpcs:disable PEAR.Commenting.FunctionComment.MissingReturn
+     * @return void|mixed
      */
     public static function deleteCookie($cookieName)
     {
