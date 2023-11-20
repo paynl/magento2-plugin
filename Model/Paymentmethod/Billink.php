e6a6c9611a566db07d7cc11c255b4f86b73324d8
kevinverschoor <61683999+kevinverschoor@users.noreply.github.com>
2023-11-20 10:28:05 +0100

    PLUG-2912 - Remove function with ObjectManager that isn't used.

--- Model/Paymentmethod/Billink.php
+++ Model/Paymentmethod/Billink.php
@@ -14,17 +14,5 @@ class Billink extends PaymentMethod
     protected function getDefaultPaymentOptionId()
     {
         return 1672;
-    }
-
-  /**
-   * @return \Magento\Framework\App\CacheInterface
-   */
-    private function getCache()
-    {
-      /** @var \Magento\Framework\ObjectManagerInterface $om */
-        $om = \Magento\Framework\App\ObjectManager::getInstance();
-      /** @var \Magento\Framework\App\CacheInterface $cache */
-        $cache = $om->get(\Magento\Framework\App\CacheInterface::class);
-        return $cache;
-    }
+    }  
 }
