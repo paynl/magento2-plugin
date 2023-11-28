3d8d28c55b37896392db2bed598e3c1037daf0fc
kevinverschoor <61683999+kevinverschoor@users.noreply.github.com>
2023-11-28 16:02:03 +0100

    Fix issue 2

--- Block/Adminhtml/Render/Credentials.php
+++ Block/Adminhtml/Render/Credentials.php
@@ -10,6 +10,7 @@ use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\UrlInterface;
 use Paynl\Payment\Helper\PayHelper;
 use Paynl\Paymentmethods;
+use Paynl\Payment\Model\Config;
 
 class Credentials extends Field
 {
@@ -40,6 +41,11 @@ class Credentials extends Field
     protected $urlInterface;
 
     /**
+     * @var Config
+     */
+    protected $_config;
+
+    /**
      * Constructor.
      *
      * @param Context $context
@@ -47,13 +53,15 @@ class Credentials extends Field
      * @param ScopeConfigInterface $scopeConfig
      * @param PayHelper $payHelper
      * @param UrlInterface $urlInterface
+     * @param Config $config
      */
-    public function __construct(Context $context, RequestInterface $request, ScopeConfigInterface $scopeConfig, PayHelper $payHelper, UrlInterface $urlInterface)
+    public function __construct(Context $context, RequestInterface $request, ScopeConfigInterface $scopeConfig, PayHelper $payHelper, UrlInterface $urlInterface, Config $config)
     {
         $this->request = $request;
         $this->scopeConfig = $scopeConfig;
         $this->payHelper = $payHelper;
         $this->urlInterface = $urlInterface;
+        $this->_config = $config;
         parent::__construct($context);
     }
 
