16459dd63a8bf5c578d9d13602b62bd94f63ec8b
kevinverschoor <61683999+kevinverschoor@users.noreply.github.com>
2024-03-26 11:34:02 +0100

    Fix version check

--- view/adminhtml/web/js/versioncheck.js
+++ view/adminhtml/web/js/versioncheck.js
@@ -1,6 +1,29 @@
 require([
     'jquery'
 ], function (jQuery) {
+
+    function compareversion(version1,version2){
+        var result=false;
+    
+        if(typeof version1 !=='object'){ version1=version1.toString().split('.'); }
+        if(typeof version2 !=='object'){ version2=version2.toString().split('.'); }
+    
+        for(var i=0;i<(Math.max(version1.length,version2.length));i++){
+    
+            if(version1[i]==undefined){ version1[i]=0; }
+            if(version2[i]==undefined){ version2[i]=0; }
+    
+            if(Number(version1[i])<Number(version2[i])){
+                result=true;
+                break;
+            }
+            if(version1[i]!=version2[i]){
+                break;
+            }
+        }
+        return(result);
+    }
+
     jQuery('#paynl_version_check_button').click(function () {
         new Ajax.Request(jQuery('#ajaxurl').text(), {
             loaderArea: false,
@@ -19,7 +42,7 @@ require([
                     var newest_version = json.version.substring(1);
                     var current_version = jQuery('#current_version').text();
 
-                    if (newest_version > current_version) {
+                    if (compareversion(current_version, newest_version)) {
                         result = 'There is a new version available (' + json.version + ')'
                     } else {
                         jQuery('#paynl_version_check_button').hide();
