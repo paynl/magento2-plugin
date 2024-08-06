293682339107f70e7ab0c874a00c27d7e48163b3
kevinverschoor <61683999+kevinverschoor@users.noreply.github.com>
2024-08-06 14:07:36 +0200

    Add guest checkout only setting

--- ViewModel/FastCheckout.php
+++ ViewModel/FastCheckout.php
@@ -2,23 +2,43 @@
 
 namespace Paynl\Payment\ViewModel;
 
-use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Customer\Model\Session;
 
 class FastCheckout implements ArgumentInterface
 {
     /**
-     * @var ScopeConfigInterface
+     * @var StoreManagerInterface
      */
-    protected $scopeConfig;
+    protected $storeManager;
+
+    /**
+     * @var Session
+     */
+    protected $session;
 
     /**
      * BuyNow constructor.
      * @param ScopeConfigInterface $scopeConfig
      */
     public function __construct(
-        ScopeConfigInterface $scopeConfig
+        StoreManagerInterface $storeManager,
+        Session $session
     ) {
-        $this->scopeConfig = $scopeConfig;
+        $this->storeManager = $storeManager;
+        $this->session = $session;
+    }
+
+    /**  
+     * @return string
+     */
+    public function getVisibility()
+    {
+        $store = $this->storeManager->getStore();
+        if($this->session->isLoggedIn() && $store->getConfig('payment/paynl_payment_ideal/fast_checkout_guest_only') == 1) {
+            return false;
+        }
+        return true;     
     }
 }
