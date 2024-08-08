956e26a67492cd7537735d5e87663205f8b89b84
woutse <wouter@pay.nl>
2024-08-08 14:05:44 +0200

    Style updates

--- view/adminhtml/web/js/configtab.js
+++ view/adminhtml/web/js/configtab.js
@@ -1,32 +1,30 @@
 require([
-  'jquery',
-  'Magento_Ui/js/modal/alert',
-  'mage/translate'
+    'jquery',
+    'Magento_Ui/js/modal/alert',
+    'mage/translate'
 ], function ($, alert, translate) {
-  $('.obscuredDisplay').click(function () {
-    $(this).parent().find('input').toggleClass('display')
-  })
+    $('.obscuredDisplay').click(function () {
+        $(this).parent().find('input').toggleClass('display')
+    })
 
-  $('#cardrefund_submit').click(function () {
-    if (parseFloat($('#cardrefund_form #refund_amount').val()) == 0 || $('#cardrefund_form #refund_amount').val().length == 0) {
-      alert({
-        content: $.mage.__('Refund amount must be greater than 0.00') + $('#cardrefund_form #currency').val()
-      })
-      return
-    }
-    if (parseFloat($('#cardrefund_form #refund_amount').val()) > parseFloat($('#cardrefund_form #refund_amount').attr('max'))) {
-      alert({
-        content: $.mage.__('Refund amount must not exceed ') + $('#cardrefund_form #refund_amount').attr('max') + ' ' + $('#cardrefund_form #currency').val()
-      })
-      return
-    }
-    if ($('#cardrefund_form #paynl_terminal').val() == 0 || $('#cardrefund_form #paynl_terminal').val().length == 0) {
-      alert({
-        content: $.mage.__('Select a terminal')
-      })
-      return
-    }
-
-    $('#cardrefund_form')[0].submit()
-  })
+    $('#cardrefund_submit').click(function () {
+        if (parseFloat($('#cardrefund_form #refund_amount').val()) == 0 || $('#cardrefund_form #refund_amount').val().length == 0) {
+            alert({
+                content: $.mage.__('Refund amount must be greater than 0.00') + $('#cardrefund_form #currency').val()
+            })
+            return false;
+        }
+        if (parseFloat($('#cardrefund_form #refund_amount').val()) > parseFloat($('#cardrefund_form #refund_amount').attr('max'))) {
+            alert({
+                content: $.mage.__('Refund amount must not exceed ') + $('#cardrefund_form #refund_amount').attr('max') + ' ' + $('#cardrefund_form #currency').val()
+            })
+            return false;
+        }
+        if ($('#cardrefund_form #paynl_terminal').val() == 0 || $('#cardrefund_form #paynl_terminal').val().length == 0) {
+            alert({
+                content: $.mage.__('Select a terminal')
+            })
+            return false;
+        }
+    })
 })
