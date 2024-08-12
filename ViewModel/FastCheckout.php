cd1db672cbfa22bfa9bf6e52fbd5d8d79264d38b
kevinverschoor <61683999+kevinverschoor@users.noreply.github.com>
2024-08-12 15:10:24 +0200

    Code Polish

--- ViewModel/FastCheckout.php
+++ ViewModel/FastCheckout.php
@@ -2,9 +2,9 @@
 
 namespace Paynl\Payment\ViewModel;
 
+use Magento\Customer\Model\Session;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Store\Model\StoreManagerInterface;
-use Magento\Customer\Model\Session;
 
 class FastCheckout implements ArgumentInterface
 {
@@ -19,8 +19,8 @@ class FastCheckout implements ArgumentInterface
     protected $session;
 
     /**
-     * BuyNow constructor.
-     * @param ScopeConfigInterface $scopeConfig
+     * @param StoreManagerInterface $storeManager
+     * @param Session $session
      */
     public function __construct(
         StoreManagerInterface $storeManager,
@@ -30,27 +30,27 @@ class FastCheckout implements ArgumentInterface
         $this->session = $session;
     }
 
-    /**  
-     * @return string
+    /**
+     * @return boolean
      */
     public function getVisibility()
     {
         $store = $this->storeManager->getStore();
-        if($this->session->isLoggedIn() && $store->getConfig('payment/paynl_payment_ideal/fast_checkout_guest_only') == 1) {
+        if ($this->session->isLoggedIn() && $store->getConfig('payment/paynl_payment_ideal/fast_checkout_guest_only') == 1) {
             return false;
         }
-        return true;     
+        return true;
     }
 
-    /**  
-     * @return string
+    /**
+     * @return boolean
      */
     public function minicartEnabled()
     {
         $store = $this->storeManager->getStore();
-        if($store->getConfig('payment/paynl_payment_ideal/fast_checkout_minicart_enabled') == 1 && $this->getVisibility()) {
+        if ($store->getConfig('payment/paynl_payment_ideal/fast_checkout_minicart_enabled') == 1 && $this->getVisibility()) {
             return true;
         }
-        return false;     
+        return false;
     }
 }
