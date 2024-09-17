9ab2d04692304eae7cd8b5ac625724d570bfbb36
Anne <anne@pay.nl>
2024-09-17 10:54:12 +0200

    Formatting

--- Model/Paymentmethod/Kunstencultuurkaart.php
+++ Model/Paymentmethod/Kunstencultuurkaart.php
@@ -4,7 +4,6 @@ namespace Paynl\Payment\Model\Paymentmethod;
 
 use Paynl\Payment\Model\Config;
 
-
 class Kunstencultuurkaart extends PaymentMethod
 {
     protected $_code = 'paynl_payment_kunstencultuurkaart';
