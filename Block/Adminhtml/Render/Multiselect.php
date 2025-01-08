5316935c0cbcce795d6a5ffa5382680e492392ad
kevinverschoor <61683999+kevinverschoor@users.noreply.github.com>
2025-01-08 12:19:32 +0100

    use _renderValue instead of render

--- Block/Adminhtml/Render/Multiselect.php
+++ Block/Adminhtml/Render/Multiselect.php
@@ -34,28 +34,14 @@ class Multiselect extends Field
     }
 
     /**
-     * Retrieve HTML markup for given form element
+     * Render element value
      *
      * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
      * @return string
      */
-    public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    public function _renderValue(\Magento\Framework\Data\Form\Element\AbstractElement $element)
     {
-        $isCheckboxRequired = $this->_isInheritCheckboxRequired($element);
-        if ($element->getInherit() == 1 && $isCheckboxRequired) {
-            $element->setDisabled(true);
-        }
-
-        if ($element->getIsDisableInheritance()) {
-            $element->setReadonly(true);
-        }
-
-        $html = '<td class="label"><label for="' .
-            $element->getHtmlId() . '"><span' .
-            $this->_renderScopeLabel($element) . '>' .
-            $element->getLabel() .
-            '</span></label></td>';
-        $html .= '<td class="value ' . ($element->getTooltip() ? 'with-tooltip' : '') . '">
+        $html = '<td class="value ' . ($element->getTooltip() ? 'with-tooltip' : '') . '">
             <span class="multiselectPay">
                 <input type="hidden" id="' . $element->getHtmlId() . '" name="' . $element->getName() . '" value="' . $element->getValue() . '" />        
                 <span class="ms_options">';
@@ -94,13 +80,7 @@ class Multiselect extends Field
 
         $html .= '</td>';
 
-        if ($isCheckboxRequired) {
-            $html .= $this->_renderInheritCheckbox($element);
-        }
-
-        $html .= $this->_renderHint($element);
-
-        return $this->_decorateRowHtml($element, $html);
+        return $html;
     }
 
 }
