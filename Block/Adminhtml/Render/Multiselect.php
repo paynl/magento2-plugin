5dc065629de7a7e914a015fe96dc3926a97b0023
kevinverschoor <61683999+kevinverschoor@users.noreply.github.com>
2025-01-10 15:48:17 +0100

    Move Html to template

--- Block/Adminhtml/Render/Multiselect.php
+++ Block/Adminhtml/Render/Multiselect.php
@@ -9,6 +9,7 @@ use Magento\Framework\Data\Form\Element\AbstractElement;
 class Multiselect extends Field
 {
     protected $configPath;
+    protected $_template = 'Paynl_Payment::system/config/multiselect.phtml';
 
     /**
      * @param Context $context
@@ -20,9 +21,7 @@ class Multiselect extends Field
     }
 
     /**
-     *
      * @param AbstractElement $element
-     *
      * @return string
      */
     protected function _getElementHtml(AbstractElement $element) // phpcs:ignore
@@ -30,57 +29,9 @@ class Multiselect extends Field
         $this->configPath = $element->getData('field_config')['config_path'];
         $this->setNamePrefix($element->getName())
             ->setHtmlId($element->getHtmlId());
+
+        $this->setData('element', $element);
+
         return $this->_toHtml();
     }
-
-    /**
-     * Render element value
-     *
-     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
-     * @return string
-     */
-    public function _renderValue(\Magento\Framework\Data\Form\Element\AbstractElement $element)
-    {
-        $html = '<td class="value ' . ($element->getTooltip() ? 'with-tooltip' : '') . '">
-            <span class="multiselectPay">
-                <input type="hidden" id="' . $element->getHtmlId() . '" name="' . $element->getName() . '" value="' . $element->getValue() . '" />        
-                <span class="ms_options">';
-
-        foreach ($element->getValues() as $value) {
-            if (!empty($value) && !empty($value['value'])) {
-                if (isset($value['is_region_visible']) && $value['is_region_visible'] === false) {
-                    continue;
-                }
-                if (is_array($value['value'])) {
-                    $label = $value['label'];
-                    $multiValues = $value['value'];
-                    $html .= '<optgroup label="' . $label . '">';
-                    foreach ($multiValues as $multiValue) {
-                        $html .= '<option class="' . ((in_array($multiValue['value'], explode(',', $element->getValue() ?? ''))) ? 'selected' : '') . '" value="' . $multiValue['value'] . '">' . $multiValue['label'] . '</option>';
-                    }
-                    $html .= '</optgroup>';
-                } else {
-                    $html .= '<option class="' . ((in_array($value['value'], explode(',', $element->getValue() ?? ''))) ? 'selected' : '') . '" value="' . $value['value'] . '">' . $value['label'] . '</option>';
-                }
-            }
-        }
-
-        $html .= '  
-                </span>
-            </span>';
-
-        if ($element->getTooltip()) {
-            $html .= '<div class="tooltip"><span class="help"><span></span></span>';
-            $html .= '<div class="tooltip-content">' . $element->getTooltip() . '</div></div>';
-        }
-
-        if ($element->getComment()) {
-            $html .= '<p class="note"><span>' . $element->getComment() . '</span></p>';
-        }
-
-        $html .= '</td>';
-
-        return $html;
-    }
-
 }
