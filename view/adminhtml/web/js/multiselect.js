8ede1ba1f4680b4e208d02c0bdf832186a62dba9
Kevin Verschoor <k.verschoor@pay.nl>
2025-01-14 14:43:24 +0100

    Fix some multiselects not working

--- view/adminhtml/web/js/multiselect.js
+++ view/adminhtml/web/js/multiselect.js
@@ -1,20 +1,19 @@
-require([
-    'jquery'
+define([
+    'jquery',
 ], function (jQuery) {
-
-    jQuery('.multiselectPay').each(function () {
-        var multiSelect = this;
-        jQuery(multiSelect).find('option').click(function () {
+    var enableMultiSelect = function (element) {
+        var multiSelect = jQuery('#' + element.element).parent();
+        multiSelect.find('option').click(function () {
             if (jQuery(this).hasClass('selected')) {
                 jQuery(this).removeClass('selected');
             } else {
                 jQuery(this).addClass('selected');
             }
-            var selectedValues = jQuery.map(jQuery(multiSelect).find('option.selected'), function (n, i) {
+            var selectedValues = jQuery.map(multiSelect.find('option.selected'), function (n, i) {
                 return n.value;
             }).join(',');
-            jQuery(multiSelect).find('input').val(selectedValues);
+            multiSelect.find('input').val(selectedValues);
         });
-    });
-
+    };
+    return enableMultiSelect;
 });
