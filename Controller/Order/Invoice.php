fa14bc20552aa4ca7827d0366c8be8579e01cc5a
Anne <anne@pay.nl>
2025-01-27 13:24:20 +0100

    Translations

--- Controller/Order/Invoice.php
+++ Controller/Order/Invoice.php
@@ -15,7 +15,7 @@ class Invoice extends \Magento\Framework\App\Action\Action
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Framework\View\Result\PageFactory $pageFactory
-    ){
+    ) {
         $this->_pageFactory = $pageFactory;
         return parent::__construct($context);
     }
