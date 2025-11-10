cddf8290bc83f6043448234ce547ac0fdfe23527
Anne <anne@pay.nl>
2025-11-10 16:51:30 +0100

    Fastcheckout CSS optional

--- Block/Checkout/FastCheckout.php
+++ Block/Checkout/FastCheckout.php
@@ -5,20 +5,42 @@ namespace Paynl\Payment\Block\Checkout;
 use Magento\Catalog\Block\Product\Context;
 use Magento\Catalog\Block\Product\ProductList\Item\Block;
 use Magento\Framework\View\Page\Config;
+use Magento\Store\Model\StoreManagerInterface;
 
 class FastCheckout extends Block
 {
     /**
+     * @var StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
      * @param Context $context
      * @param Config $page
+     * @param StoreManagerInterface $storeManager
      * @param array $data
      */
     public function __construct(
         Context $context,
         Config $page,
+        StoreManagerInterface $storeManager,
         array $data = []
     ) {
-        $page->addPageAsset('Paynl_Payment::css/payFastCheckout.css');
+        $this->storeManager = $storeManager;
+
+        if ($this->isCssEnabled()) {
+            $page->addPageAsset('Paynl_Payment::css/payFastCheckout.css');
+        }
+        
         parent::__construct($context, $data);
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isCssEnabled()
+    {
+        $store = $this->storeManager->getStore();
+        return $store->getConfig('payment/paynl_payment_ideal/fast_checkout_css_enabled') == 1;
     }
 }
