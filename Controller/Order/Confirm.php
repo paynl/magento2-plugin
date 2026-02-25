114fdb1628263648dcb9ad16ae0d34317c15840c
Kevin Verschoor <k.verschoor@pay.nl>
2026-02-25 11:35:29 +0100

    PLUG-5328 - Redirect when controller files are called without checkoutSession

--- Controller/Order/Confirm.php
+++ Controller/Order/Confirm.php
@@ -5,26 +5,34 @@ namespace Paynl\Payment\Controller\Order;
 class Confirm extends \Magento\Framework\App\Action\Action
 {
     protected $_pageFactory;
+    protected $checkoutSession;
 
     /**
      * Pending construct
      *
      * @param \Magento\Framework\App\Action\Context $context
      * @param \Magento\Framework\View\Result\PageFactory $pageFactory
+     * @param \Magento\Checkout\Model\Session $checkoutSession
      */
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $pageFactory
+        \Magento\Framework\View\Result\PageFactory $pageFactory,
+        \Magento\Checkout\Model\Session $checkoutSession
     ) {
         $this->_pageFactory = $pageFactory;
+        $this->checkoutSession = $checkoutSession;
         return parent::__construct($context);
     }
 
     /**
-     * @return object
+     * @return object|void
      */
     public function execute()
     {
+        if (empty($this->checkoutSession->getLastRealOrderId())) {
+            $this->_redirect('checkout/cart');
+            return;
+        }
         return $this->_pageFactory->create();
     }
 }
