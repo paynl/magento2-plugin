66e150438bf17e1c8c5e4b5350936be6a4b9b649
Kevin Verschoor <k.verschoor@pay.nl>
2026-05-08 15:42:43 +0200

    PLUG-5575  - Remove unused CRSF in adminHTML

--- Controller/Adminhtml/Order/CardRefund.php
+++ Controller/Adminhtml/Order/CardRefund.php
@@ -2,38 +2,17 @@
 
 namespace Paynl\Payment\Controller\Adminhtml\Order;
 
-use Magento\Framework\App\RequestInterface;
-use Magento\Framework\App\Request\InvalidRequestException;
 use Magento\Payment\Helper\Data as PaymentHelper;
 use Magento\Quote\Model\QuoteRepository;
 use Magento\Sales\Model\OrderRepository;
-use Paynl\Payment\Controller\CsrfAwareActionInterface;
 use Paynl\Payment\Helper\PayHelper;
 
-class CardRefund extends \Magento\Backend\App\Action implements CsrfAwareActionInterface
+class CardRefund extends \Magento\Backend\App\Action
 {
     private $orderRepository;
     private $quoteRepository;
     private $paymentHelper;
     protected $resultFactory;
-
-    /**
-     * @param RequestInterface $request
-     * @return boolean
-     */
-    public function createCsrfValidationException(RequestInterface $request): ?InvalidRequestException
-    {
-        return null;
-    }
-
-    /**
-     * @param RequestInterface $request
-     * @return boolean
-     */
-    public function validateForCsrf(RequestInterface $request): bool
-    {
-        return true;
-    }
 
     /**
      *
@@ -76,7 +55,7 @@ class CardRefund extends \Magento\Backend\App\Action implements CsrfAwareActionI
         $params = $this->getRequest()->getParams();
         $orderId = isset($params['order_id']) ? strip_tags($params['order_id']) : null;
         $returnUrl = isset($params['return_url']) ? urldecode($params['return_url']) : null;
-        $refundAmount = isset($params['refund_amount']) ? (float)$params['refund_amount'] : 0;
+        $refundAmount = isset($params['refund_amount']) ? (float) $params['refund_amount'] : 0;
         $terminalId = isset($params['paynl_terminal']) ? strip_tags($params['paynl_terminal']) : null;
         $redirectUrl = '';
 
